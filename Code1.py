c1f53c73a13e6cbb536b8ecd79d0eafce611d0c1
sayonibid <sayonibid@gmail.com>
2026-02-18 13:20:57 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,13 @@
+from machine import Pin
+import time
 
+sm=PWM(Pin(4),freq=50)
+list=[35,70,120]
+
+while True:
+    sm.duty(list[0])
+    time.sleep(1)
+    sm.duty(list[1])
+    time.sleep(1)
+    sm.duty(list[2])
+    time.sleep(1)
